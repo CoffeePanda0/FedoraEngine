4d0e317d1c4ee4033d45f4f2e327ffa9deb6a98b
Alexander Thomas <34898787+CoffeePanda0@users.noreply.github.com>
2020-07-16 22:40:37 +0100

    Add files via upload
    added fps counter

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 //Main file to load other functions
 #include "game.h"
-
+#include <string.h>
 /* TODO:
-render all game objects 
-haha memory leak go BRRRRRRRRRRRRRRRRRRRR
+game objects
 fps counter
 smooth movement
 multiple inputs at once
@@ -14,12 +13,16 @@ everything lol
 
 const int maxFPS = 60;
 const int frame_delay = 1000 / maxFPS;
-
+char buffer[20];
 Uint32 frame_start;
 int frame_time;
 
 int main(int argc, char* argv[])
 {
+	Uint32 fps_lasttime = SDL_GetTicks(); //the last recorded time.
+	Uint32 fps_current; //the current FPS.
+	Uint32 fps_frames = 0; //frames passed since the last recorded fps.
+
 	log_init();
 
 	init("FedoraEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, false);
@@ -33,10 +36,19 @@ int main(int argc, char* argv[])
 		Update();
 		event_handler();
 
-		frame_start = SDL_GetTicks() - frame_start;
+		fps_frames++;
+   		if (fps_lasttime < SDL_GetTicks() - 1000)
+   		{
+      		fps_lasttime = SDL_GetTicks();
+      		fps_current = fps_frames;
+      		fps_frames = 0;
+			printf("FPS: %s \n", itoa(fps_current, buffer, 10));
+   		}
+		
+		/*frame_start = SDL_GetTicks() - frame_start;
 
 		if (frame_delay > frame_time)
-			SDL_Delay(frame_delay - frame_time);
+			SDL_Delay(frame_delay - frame_time); */
 	}
 	Clean();
 
