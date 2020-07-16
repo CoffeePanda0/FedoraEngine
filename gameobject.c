ef7a871dd80abf7892ac0b8413cbcc78107ec149
Alexander Thomas <34898787+CoffeePanda0@users.noreply.github.com>
2020-07-16 22:42:24 +0100

    Add files via upload

--- gameobject.c
+++ gameobject.c
@@ -1,8 +1,26 @@
+// basic gameobject functions
 #include "game.h"
+
+struct ObjList {
+	struct GameObject *obj;
+	struct ObjList *next;
+}; 
+
+struct ObjList *objlist;
+
+void push (struct GameObject *obj)
+{
+	struct ObjList *newobj;
+	newobj = malloc(sizeof(*newobj));
+	newobj->obj = obj;
+	newobj->next = objlist;
+	objlist = newobj;
+}
 
 void CreateObject(int xPos, int yPos, int height, int width, const char* textPath, struct GameObject *obj)
 {
 	// does this cause a memory leak.. yes it does somewhere i think
+	struct objList *objList;
 	SDL_Rect objRect;
 	SDL_Texture* texture;
 
@@ -11,14 +29,33 @@ void CreateObject(int xPos, int yPos, int height, int width, const char* textPat
 	obj->objRect.w = width;
 	obj->objRect.h = height;
 	obj->texture = TextureManager(textPath, renderer);
+	push(obj);
 }
 
-void RenderObject(struct GameObject obj)
-{
-	SDL_RenderCopy(renderer, obj.texture, NULL, &obj.objRect);
+void RenderObject() {
+	for (struct ObjList *o = objlist; o; o = o->next)
+		SDL_RenderCopy(renderer, o->obj->texture, NULL, &o->obj->objRect);
 }
 
-void DestroyObject(struct GameObject *obj)
-{
-	SDL_DestroyTexture(obj->texture);
+void DestroyObject(struct GameObject* obj) {
+	struct ObjList* tmp;
+	if (objlist->obj == obj) {
+		tmp = objlist;
+		objlist = objlist->next;
+		SDL_DestroyTexture(tmp->obj->texture);
+		SDL_free(tmp->obj)
+			SDL_free(tmp);
+		return;
+	}
+	for (struct ObjList* o = objlist; o; o = o->next) {
+		if (o->next == obj) {
+			tmp = o->next;
+			o->next = o->next->next;
+			SDL_DestroyTexture(tmp->obj->texture);
+			SDL_free(tmp->obj)
+				SDL_free(tmp);
+			return;
+		}
+	}
+	error("DestroyObject: Object not found!");
 }
--- gameobject.h
+++ gameobject.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "SDL.h"
 
+
 struct GameObject {
 	SDL_Rect objRect;
 	SDL_Texture* texture;
 };
 
-void RenderObject(struct GameObject obj);
+void RenderObject();
 void DestroyObject(struct GameObject *obj);
 void CreateObject(int xPos, int yPos, int height, int width, const char* textPath, struct GameObject *obj);
