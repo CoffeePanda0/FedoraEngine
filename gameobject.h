0d559597efa92b776b7b7db548ffb2bf0f80af53
Alexander Thomas <34898787+CoffeePanda0@users.noreply.github.com>
2020-08-05 22:08:50 +0100

    Add files via upload
    Added direction Enums for ground collision

--- gameobject.h
+++ gameobject.h
@@ -7,6 +7,17 @@ struct GameObject {
 	SDL_Texture* texture;
 };
 
+enum CollDir {
+	DIR_LEFT,
+	DIR_RIGHT,
+	DIR_ABOVE,
+	DIR_BELOW,
+	DIR_NONE,
+};
+
+extern enum CollDir dir;
+
 void RenderObject();
+void CollisionDetection();
 void DestroyObject(struct GameObject *obj);
 void CreateObject(int xPos, int yPos, int height, int width, const char* textPath, struct GameObject *obj);
